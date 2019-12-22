ee810ad645bfd1a900578c8a49ec32b9793f161d
Evgenii Davydov <davydes@gmail.com>
2019-12-22 22:46:06 +0300

    add descriptions

--- src/main/kotlin/statistic/Window.kt
+++ src/main/kotlin/statistic/Window.kt
@@ -2,8 +2,23 @@ package statistic
 
 import java.time.Duration
 
+/*
+  Abstract window of any objects
+*/
 interface Window<T> {
+    /*
+      Whole window statistic value
+    */
     fun value(): T
+
+    /*
+      Value for particular duration, but less than window size.
+      Precision limited by window size unit.
+    */
     fun value(duration: Duration): T
+
+    /*
+      Add new value to statistic window
+    */
     fun add(value: T): Window<T>
 }
